f1f644bf38d09a709fa821206f0d3d9e00249cf6
Ryan Blake <ryan.blake@gmail.com>
2024-06-29 23:57:45 -0400

    Font

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div class="px-4 py-8 mx-auto">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+      <div class="max-w-screen-md text-pretty text-lg mx-auto flex flex-col items-center justify-center">
         Coming soon!
       </div>
     </div>
