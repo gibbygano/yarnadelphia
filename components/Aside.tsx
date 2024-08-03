832d741f17e21f5f087d7ad404c27b2d06ec9913
Ryan Blake <ryan.blake@gmail.com>
2024-08-03 16:39:49 -0400

    Fixing shitty dumb instagram embed

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -3,13 +3,13 @@ export default () => {
         <>
             <blockquote
                 class="instagram-media"
-                data-instgrm-permalink="https://www.instagram.com/elsha007/?utm_source=ig_embed&amp;utm_campaign=loading"
+                data-instgrm-permalink="https://www.instagram.com/yarnadelphia/?utm_source=ig_embed&amp;utm_campaign=loading"
                 data-instgrm-version="14"
                 style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"
             >
                 <div style="padding:16px;">
                     <a
-                        href="https://www.instagram.com/elsha007/?utm_source=ig_embed&amp;utm_campaign=loading"
+                        href="https://www.instagram.com/yarnadelphia/?utm_source=ig_embed&amp;utm_campaign=loading"
                         style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;"
                         target="_blank"
                     >
@@ -31,7 +31,7 @@ export default () => {
                                 viewBox="0 0 60 60"
                                 version="1.1"
                                 xmlns="https://www.w3.org/2000/svg"
-                                xmlns:xlink="https://www.w3.org/1999/xlink"
+                                xmlnsXlink="https://www.w3.org/1999/xlink"
                             >
                                 <g
                                     stroke="none"
@@ -90,18 +90,18 @@ export default () => {
                     </a>
                     <p style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;">
                         <a
-                            href="https://www.instagram.com/elsha007/?utm_source=ig_embed&amp;utm_campaign=loading"
+                            href="https://www.instagram.com/yarnadelphia/?utm_source=ig_embed&amp;utm_campaign=loading"
                             style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px;"
                             target="_blank"
                         >
                             Rebecca
                         </a>{" "}
                         (@<a
-                            href="https://www.instagram.com/elsha007/?utm_source=ig_embed&amp;utm_campaign=loading"
+                            href="https://www.instagram.com/yarnadelphia/?utm_source=ig_embed&amp;utm_campaign=loading"
                             style=" color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px;"
                             target="_blank"
                         >
-                            elsha007
+                            YARNADELPHIA
                         </a>) • Instagram photos and videos
                     </p>
                 </div>
