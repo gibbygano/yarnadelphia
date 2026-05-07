ba1ba02e8e0f572f015e06c06c804c4c83d459b3
Ryan J. Blake <ryan.blake@gmail.com>
2026-05-07 13:33:51 -0400

    stuff to trigger pipeline

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,17 +1,23 @@
-const Card = () => {
+interface CardProps {
+  title: string;
+  text: string;
+  imgSrc: string;
+  imgAlt: string;
+}
+
+const Card = ({ title, text, imgSrc, imgAlt }: CardProps) => {
   return (
     <div class="card bg-base-100 w-96 shadow-sm">
       <div class="card-body">
-        <h2 class="card-title">Card Title</h2>
+        <h2 class="card-title">{title}</h2>
         <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
+          {text}
         </p>
       </div>
       <figure>
         <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
+          src={imgSrc}
+          alt={imgAlt}
         />
       </figure>
     </div>
